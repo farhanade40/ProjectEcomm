f05b84c58bb49fcfe0ea88dfd96568198de32522
farhanaderenaldi <farhanade40@gmail.com>
2021-04-02 21:39:19 -0800

    second commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://127.0.0.1:8080/projectweb/ci_hmvc/';
+
 
 /*
 |--------------------------------------------------------------------------
@@ -324,7 +325,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'tutoriallogin';
 
 /*
 |--------------------------------------------------------------------------
